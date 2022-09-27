5ef5881d9d501c42944ee97b0615f8ddecb5ff33
Logan Becket <logan.becket@planjupiter.com>
2022-09-26 23:59:42 -0500

    meaningless change

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,5 @@
-import CategoryItem from "../category-item/category-item.component";
 import "./directory.styles.scss";
+import CategoryItem from "../category-item/category-item.component";
 
 const Directory = ({ categories }) => {
   return (
